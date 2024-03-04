e458b4fffc9f62bc6d3d548b3ad0cdd7a60a273b
Maza455 <same7nte@gmail.com>
2024-03-04 16:18:09 +0200

    Adding a folder structure for Capstone project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+export default {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2024": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+
+    }
+};
